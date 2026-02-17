c350a97a688dc9868ebc15161837cc5e45bb9630
thisisaktar <ska144993@gmail.com>
2026-02-17 15:40:02 +0530

    Disabled SMTP for Render fix

--- app/utils.py
+++ app/utils.py
@@ -10,26 +10,28 @@ from psycopg2.extras import RealDictCursor
 
 
 
+def send_otp_email(email, otp):
+    print(f"OTP for {email} is {otp}")
 
 
-def send_otp_email(receiver_email, otp):
+# def send_otp_email(receiver_email, otp):
 
-    sender_email = os.getenv("MAIL_USER")
-    sender_password = os.getenv("MAIL_PASS")
+#     sender_email = os.getenv("MAIL_USER")
+#     sender_password = os.getenv("MAIL_PASS")
 
-    subject = "Your OTP Verification Code"
-    body = f"Your OTP code is: {otp}\n\nIt expires in 5 minutes."
+#     subject = "Your OTP Verification Code"
+#     body = f"Your OTP code is: {otp}\n\nIt expires in 5 minutes."
 
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
+#     msg = MIMEText(body)
+#     msg['Subject'] = subject
+#     msg['From'] = sender_email
+#     msg['To'] = receiver_email
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(sender_email, sender_password)
-    server.send_message(msg)
-    server.quit()
+#     server = smtplib.SMTP("smtp.gmail.com", 587)
+#     server.starttls()
+#     server.login(sender_email, sender_password)
+#     server.send_message(msg)
+#     server.quit()
 
 
 
